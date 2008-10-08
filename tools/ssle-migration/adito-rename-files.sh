34e6cd4edf326faf783c74659bdc38ad9c748f16
mattock <mattock@7e7e6418-0a4e-0410-99f2-a06e816c90e7>
2008-10-08 15:19:06 +0000

    Added a few SSL-Explorer -> Adito migration scripts to $ADITO_HOME/tools/ssle-migration/

---
+++ tools/ssle-migration/adito-rename-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Rename files and directories. Please make sure that replace_text_in_filename.sh
+# is in PATH
+
+# Move down one directory level at a time so that nothing breaks when moving things around.
+for LEVEL in 1 2 3 4 5 6 7 8 9; do
+	find . -maxdepth $LEVEL -exec replace_text_in_filename.sh -o sslexplorer -n adito -f {} \; 
+done
+
+exit
---
+++ tools/ssle-migration/adito-rename-text.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# This is run with the original filename as an argument. If you need convert
+# many files, you should use "find" similarly to this:
+#
+# find /directory -type f -exec ./adito-rename-text.sh {} \;
+
+# Check if this file matches, skipping binary files. If it does not match, then exit.
+grep -i --binary-files=without-match explorer $1 > /dev/null
+if [ $? -ne 0 ]; then
+	exit
+fi
+
+# Remove most variants of SSL-Explorer's name and references to 3sp
+cat $1 |sed s/"sslexplorer"/"adito"/g|sed s/SSL-Explorer/Adito/g|sed s/"SSL Explorer"/"Adito"/g|sed s/sslExplorer/adito/g|sed s/SSLEXPLORER/ADITO/g|sed s/SSLExplorer/Adito/g|sed s/ssl-explorer/adito/g|sed s/SslExplorer/Adito/g|sed s/SSl-Explorer/Adito/g|sed s/3sp\.com/localhost/g > $1.new
+
+mv $1.new $1
+rm -f $1.new
