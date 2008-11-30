7fcb80367a455e642e25442c07f815c1d7961f23
mattock <mattock@7e7e6418-0a4e-0410-99f2-a06e816c90e7>
2008-11-30 12:55:17 +0000

    Changed the hardcoded Extension store path to point to www.aditoproject.org/appstore

--- adito/src/com/adito/extensions/store/ExtensionStore.java
+++ adito/src/com/adito/extensions/store/ExtensionStore.java
@@ -91,7 +91,7 @@ import com.adito.util.ZipExtract;
  */
 public class ExtensionStore {
 
-    public static final String HTTP_3SP_COM_APPSTORE = "http://download.localhost/appstore/";
+    public static final String HTTP_3SP_COM_APPSTORE = "http://www.aditoproject.org/appstore/";
     private static final Log log = LogFactory.getLog(ExtensionStore.class);
 	private static final String DIRS_TO_REMOVE = "dirsToRemove";
 	private static String currentEdition = "Community Edition";
