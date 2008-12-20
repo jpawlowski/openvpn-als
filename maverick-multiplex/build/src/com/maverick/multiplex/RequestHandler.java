11913b500e73b140fba0bc5e185e9bd2f1f63565
svetzal <svetzal@7e7e6418-0a4e-0410-99f2-a06e816c90e7>
2008-12-20 20:46:56 +0000

    more cleaning up

--- maverick-multiplex/build/src/com/maverick/multiplex/RequestHandler.java
+++
@@ -1,6 +0,0 @@
-package com.maverick.multiplex;
-
-public interface RequestHandler {
-	public boolean processRequest(Request request, MultiplexedConnection connection);
-	public void postReply(MultiplexedConnection connection);
-}
