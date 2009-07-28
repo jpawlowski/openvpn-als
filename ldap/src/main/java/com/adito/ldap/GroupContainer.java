ad12fc51537fa1b1450bd304d906df00f44262c4
mattock <mattock@7e7e6418-0a4e-0410-99f2-a06e816c90e7>
2009-07-28 10:32:59 +0000

    Added sources to ldap in nonembedded

---
+++ ldap/src/main/java/com/adito/ldap/GroupContainer.java
@@ -0,0 +1,81 @@
+
+				/*
+ *  Adito
+ *
+ *  Copyright (C) 2003-2006 3SP LTD. All Rights Reserved
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+			
+package com.adito.ldap;
+
+import com.adito.policyframework.PrincipalCache;
+import com.adito.security.Role;
+import org.apache.commons.cache.Cache;
+
+import java.util.Collection;
+
+public final class GroupContainer extends PrincipalCache<LdapGroup> {
+
+    private static final String GROUPS_CACHE_PREFIX = "groups";
+    private static final String MESSAGE_BUNDLE = "ldap";
+    private static final String CACHE_FULL_MESSAGE = "ldap.cache.group.full";
+    public static final GroupContainer EMPTY_CACHE = new GroupContainer(0, true);
+    
+    private final Cache dnToRoleCache;
+    
+    GroupContainer(int cacheSize, boolean inMemoryCache) {
+        super(cacheSize, inMemoryCache, false, "role", MESSAGE_BUNDLE, CACHE_FULL_MESSAGE);
+        dnToRoleCache = createCache(MESSAGE_BUNDLE, CACHE_FULL_MESSAGE);
+    }    
+    
+    synchronized Role getGroupByDn(String dn) {
+        return (Role) dnToRoleCache.retrieve(dn.toLowerCase());
+    }
+
+    synchronized boolean containsDn(String dn) {
+        return getGroupByDn(dn) != null;
+    }
+
+    synchronized String storeGroup(LdapGroup group) {
+        String principalName = storePrincipal(group);
+        final String dn = group.getDn().toLowerCase();
+        dnToRoleCache.store(dn, group, Long.MAX_VALUE, null, GROUPS_CACHE_PREFIX);
+        return principalName;
+    }
+
+
+
+    
+    synchronized void updateRemovedGroups(Collection<String> missingGroups) {
+        for (String groupName : missingGroups) {
+            LdapGroup group = retrievePrincipal(groupName);
+            if (group != null) {
+                removeGroup(group);           
+            }
+        }
+    }
+
+    public synchronized void removeGroup(LdapGroup group) {
+        removePrincipal(group);
+        final String dn = group.getDn().toLowerCase();
+        dnToRoleCache.store(dn, null, 0L, null, GROUPS_CACHE_PREFIX);
+
+    }
+    
+    public synchronized void close() {
+        super.close();
+        closeCache(dnToRoleCache);
+    }
+}
---
+++ ldap/src/main/java/com/adito/ldap/LdapCommunityPlugin.java
@@ -0,0 +1,47 @@
+/*
+ *  Adito
+ *
+ *  Copyright (C) 2003-2006 3SP LTD. All Rights Reserved
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package com.adito.ldap;
+
+import com.adito.core.UserDatabaseManager;
+import com.adito.extensions.ExtensionDescriptor;
+import com.adito.extensions.ExtensionException;
+import com.adito.extensions.types.DefaultPlugin;
+import com.adito.extensions.types.PluginDefinition;
+import com.adito.security.UserDatabaseDefinition;
+import org.jdom.Element;
+
+/**
+ * Adds a Ldap <i>User Database</i> to Adito.
+ */
+public class LdapCommunityPlugin extends DefaultPlugin {
+
+
+    /**
+     * Constructor.
+     */
+    public LdapCommunityPlugin() {
+        super(null, false);
+    }
+
+    public void startPlugin(PluginDefinition definition, ExtensionDescriptor descriptor, Element element) throws ExtensionException {
+        super.startPlugin(definition, descriptor, element);
+        UserDatabaseDefinition databaseDefinition = new UserDatabaseDefinition(LdapUserDatabase.class, "ldap", "ldap", 90);
+        UserDatabaseManager.getInstance().registerDatabase(databaseDefinition);
+    }
+}
