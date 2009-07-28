0a5108092f465f803f7022201ff09b4d4634fe66
mattock <mattock@7e7e6418-0a4e-0410-99f2-a06e816c90e7>
2009-07-28 11:08:06 +0000

    Mavenized ldap and pam modules in nonembedded. Cleaned up directory structure of radius module.

---
+++ pam/src/main/java/com/adito/pam/PAMGroup.java
@@ -0,0 +1,65 @@
+/*
+ *  Adito-PAM
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package com.adito.pam;
+
+import java.io.Serializable;
+
+import com.adito.realms.Realm;
+import com.adito.security.Role;
+
+/**
+ * This is the PAM Role implementation
+ *
+ */
+public class PAMGroup implements Role<PAMGroup>, Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String principalName;
+	private Realm realm;
+
+	/**
+	 * @param principalName Group Name
+	 * @param realm Realm
+	 * Default constructor
+	 */
+	public PAMGroup(String principalName, Realm realm) {
+		this.principalName = principalName;
+		this.realm = realm;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.adito.policyframework.Principal#getPrincipalName()
+	 */
+	public String getPrincipalName() {
+		return principalName;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.adito.policyframework.Principal#getRealm()
+	 */
+	public Realm getRealm() {
+		return realm;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	public int compareTo(PAMGroup o) {
+		return getPrincipalName().compareTo(o.getPrincipalName());
+	}
+
+}
